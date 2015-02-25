cab25906d31f35bd7c55daf9f1eedc0f5572e554
Julien Eluard <julien.eluard@gmail.com>
2015-02-25 15:05:13 -0300

    Allow to provide browsers argument. Fixes #1.

--- src/danielsz/autoprefixer.clj
+++ src/danielsz/autoprefixer.clj
@@ -13,13 +13,18 @@
        (core/by-name files)
        (map (juxt core/tmppath core/tmpfile identity))))
 
-(core/deftask autoprefixer [f files FILES [str] "A vector of filenames to process with autoprefixer."]
+(core/deftask autoprefixer
+  [f files    FILES       [str] "A vector of filenames to process with autoprefixer."
+   b browsers BROWSERS    str   "A string describing browsers autoprefixer will target."]
   (let [tmp-dir (core/temp-dir!)]
     (core/with-pre-wrap fileset
       (doseq [[in-path in-file file] (find-css-files fileset files)]
         (boot.util/info "Autoprefixing %s\n" (:path file))
         (let [out-file (doto (io/file tmp-dir in-path) io/make-parents)]
-          (util/dosh "autoprefixer" (.getPath in-file) "-o" (.getPath out-file))))
+          ; See https://github.com/boot-clj/boot/issues/141
+          (if browsers
+            (util/dosh "autoprefixer" (.getPath in-file) "-o" (.getPath out-file) "-b" browsers)
+            (util/dosh "autoprefixer" (.getPath in-file) "-o" (.getPath out-file)))))
       (-> fileset
           (core/add-resource tmp-dir)
           core/commit!))))
