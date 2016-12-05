bd55f27091efaad8e20419202325bddd8db75afb
David Whittington <djwhitt@gmail.com>
2016-12-05 15:31:48 -0600

    Add support for specifying postcss executable path

--- src/danielsz/autoprefixer.clj
+++ src/danielsz/autoprefixer.clj
@@ -14,17 +14,18 @@
        (map (juxt core/tmp-path core/tmp-file identity))))
 
 (core/deftask autoprefixer
-  [f files    FILES       [str] "A vector of filenames to process with autoprefixer."
-
-   b browsers BROWSERS    str   "A string describing browsers autoprefixer will target.
+  [p exec-path PATH        str   "PostCSS executable path (defaults to \"postcss\" if omitted)."
+   f files     FILES       [str] "A vector of filenames to process with autoprefixer."
+   b browsers  BROWSERS    str   "A string describing browsers autoprefixer will target.
    Eg:  \"last 1 version, > 5%\".
    More details at https://github.com/ai/browserslist"]
   (let [tmp-dir (core/tmp-dir!)]
     (core/with-pre-wrap fileset
       (doseq [[in-path in-file file] (find-css-files fileset files)]
         (boot.util/info "Autoprefixing %s\n" (:path file))
-        (let [out-file (doto (io/file tmp-dir in-path) io/make-parents)]
-          (apply util/dosh "postcss" "--use" "autoprefixer"
+        (let [out-file (doto (io/file tmp-dir in-path) io/make-parents)
+              postcss (or exec-path "postcss")]
+          (apply util/dosh postcss "--use" "autoprefixer"
                  (.getPath in-file)
                  "-o" (.getPath out-file)
                  (when browsers ["--autoprefixer.browsers" browsers]))))
